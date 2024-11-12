ecd5f158e6119ea17eb69fbba40be1727ed31e9e
Oscar Atle Brovold <oscarab@ulrik.uio.no>
2024-11-12 11:02:53 +0100

    Jobba videre med H22

--- EKSAMEN/H22/answers.sql
+++ EKSAMEN/H22/answers.sql
@@ -24,3 +24,45 @@ WHERE tg.navn = 'Oslo Open';
 -- alle rader fra parti-tabellen for partiene som har resultat 'remis' og som er
 -- tilknyttet turneringen med navn 'Sjakkelakke'.
 
+DELETE 
+FROM parti p
+WHERE p.tid = (SELECT t.tid FROM turnering t WHERE t.navn = 'Sjakkelakke')
+AND p.vinner = 'remis';
+
+-- Oppgave 2.5 Resultater
+-- Skriv en SQL-kommando som lager et VIEW med navn resultat som inneholder
+-- resultater av partier for hver spiller. Det skal inneholde tre kolonner: pid for
+-- partiet, sid for spilleren, og en kolonne med navn utfall som enten er 'vant',
+-- 'tapte' eller 'remis' avhengig av om spilleren henholdsvis vant partiet, tapte
+-- partiet, eller partiet ble remis/uavgjort.
+
+CREATE VIEW resultat(pid, sid, utfall) AS
+SELECT pid, hvit, 'vant'
+FROM parti 
+WHERE vinner = 'hvit'
+UNION
+SELECT pid, sort, 'vant'
+FROM parti
+WHERE vinner = 'sort'
+UNION 
+SELECT pid, hvit 'tapte'
+FROM parti 
+WHERE vinner = 'sort'
+...
+
+-- Oppgave 2.6 Vinnere
+-- I sjakkturneringene får man 1 poeng hver gang man vinner et parti og 0 poeng
+-- om man taper. Dersom et parti ender med remis (uavgjort) vil hver spiller få
+-- 0.5 poeng hver. Vinneren av turneringen er den med flest poeng. Dersom det er
+-- uavgjort legges det til ekstra partier for de med flest poeng, slik at det alltid
+-- ender med at én har flest poeng i hver turnering.
+-- Skriv en spørring som finner vinneren på hver turnering. Spørringen skal
+-- returnere navn på turneringen og navnet på spilleren som vant turneringen. Du
+-- kan bruke viewet fra forrige oppgave om du ønsker.
+
+
+
+
+
+
+
